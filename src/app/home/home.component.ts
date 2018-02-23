8ff15a50f08e9c2a36d8ff943e236b0dd24d380c
Mujib517 <mujib517@gmail.com>
2018-02-23 07:24:39 +0530

    ngFor, ngClass and ngStyle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,27 +3,43 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-home',
     template: `
-    <div *ngIf="show">
-        ...
-        ...
-        <h1 >Home Page</h1>
-    </div>
+       <table border="1">
+            <tr>
+                <td>Value</td>
+                <td>Index</td>
+                <td>Even</td>
+                <td>Odd</td>
+                <td>First</td>
+                <td>Last</td>
+            </tr>
+            <tr [ngClass]="{fg:evn,fg2:odd }" *ngFor="let val of arr;let idx=index;let evn=even;let odd=odd;let fst=first;let lst=last">
+                <td>{{val}}</td>
+                <td>{{idx}}</td>
+                <td>{{evn}}</td>
+                <td>{{odd}}</td>
+                <td>{{fst}}</td>
+                <td>{{lst}}</td>
+            </tr>
+       </table>   
 
+        <h1 [ngClass]="cls">Home Page</h1>
+        <h1 [ngStyle]="style">heading</h1>
 
-    <div [ngSwitch]="priority">
-        <h1 *ngSwitchCase="'high'">High priority Task</h1>
-        <h1 *ngSwitchCase="'medium'">Medium priority Task</h1>
-        <h1 *ngSwitchCase="'low'">Low priority Task</h1>
-        <h1 *ngSwitchDefault>Very Low Priority Task</h1>
-    </div>
-
-    <button (click)="toggle()">Toggle</button>
     `
 })
 export class HomeComponent {
     show: boolean = true;
     priority: string = "very low";
-   
+    arr: number[] = [1, 2, 3, 4, 5, 6];
+    cls: any = {
+        fg: true,
+        bg: true
+    };
+    style: any = {
+        color: 'yellow',
+        'background-color': 'blue'
+    }
+
 
     toggle(): void {
         if (this.show) {
