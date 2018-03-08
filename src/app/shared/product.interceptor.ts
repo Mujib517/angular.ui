46a5badd34385681872e94fd23e14ea5c28b6a73
Mujib517 <mujib517@gmail.com>
2018-03-08 07:43:50 +0530

    Interceptors contd.

--- src/app/shared/product.interceptor.ts
+++ src/app/shared/product.interceptor.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class ProductInterceptor implements HttpInterceptor {
@@ -10,9 +13,18 @@ export class ProductInterceptor implements HttpInterceptor {
         var token = localStorage.getItem("token");
         if (token) {
             var newReq = req.clone({ headers: req.headers.set("authorization", token) });
-            return next.handle(newReq);
+            return next.handle(newReq)
+                .do(
+                response => {
+                    console.log("In response interceptor");
+                    return response;
+                })
+                .catch(err => {
+                    console.log("In Error");
+                    return Observable.throw(err)
+                });
         }
-        
+
         return next.handle(req);
     }
 }
