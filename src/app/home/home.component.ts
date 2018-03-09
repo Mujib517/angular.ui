1d74f054eca76b0d2179e16b4cb5c3f011131c54
Mujib517 <mujib517@gmail.com>
2018-03-09 07:37:21 +0530

    Change detection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,34 @@
 import { Component } from "@angular/core";
 import { ConsoleLogger } from "../shared/console.logger";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
     selector: 'app-home',
     template: `
        <h1>Home Page</h1>
-       <h1 [show]="showEl">Heading</h1>
-        <h1 *if="showEl">IF directive demo</h1>
-
-       <h1 [show]="!showEl">Heading2</h1>
+      
+       <h1>{{count}}</h1>
+       <button (click)="inc()">++</button>
     `
 })
 export class HomeComponent {
 
-    showEl: boolean = true;
+    count: number = 0;
 
-    constructor(private logger: ConsoleLogger) {
-        logger.warn("COmponent took a lot of time to load");
+    constructor(private logger: ConsoleLogger, private http: HttpClient) {
+        logger.warn("Component took a lot of time to load");
+        http.get("https://api.github.com/users")
+            .subscribe(
+            () => this.count = 1000,
+            () => this.count = 100000
+            )
+        //setInterval(() => this.count++, 1000);
+    }
+
+    inc() {
+        this.count++;
     }
 }
 
+// asynchronous op   events,timers,web svc
 //ngIf, ngFor, ngSwitch, ngModel, ngClass ngStyle
